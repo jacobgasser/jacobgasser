6c1a557a15d02ba905e8f8ab2606e05a0f77e997
Jacob Gasser <me@jacobgasser.com>
2019-06-17 12:38:33 -0400

    Now I can choose the port in the command line when running server.exe

--- gostuff/main/main.go
+++ gostuff/main/main.go
@@ -6,27 +6,33 @@ import (
 	"os"
 	"path/filepath"
 )
+
 var pathstring string = ""
+
 func main() {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
+	port := "80"
+	if os.Args[0] != "" {
+		port = os.Args[1]
+	}
 	exPath := filepath.Dir(ex)
 	pathstring = exPath
 	fmt.Println(exPath)
 	fmt.Println("jacobgasser's webserver is now online!")
-	http.Handle("/", http.FileServer(http.Dir(pathstring + "/src")))
+	http.Handle("/", http.FileServer(http.Dir(pathstring+"/src")))
 	http.HandleFunc("/help", index)
-	http.ListenAndServe(":80", nil)
+	fmt.Println("Starting on port " + port)
+	errr := http.ListenAndServe(":"+port, nil)
+	if errr != nil {
+		panic(errr)
+	}
 
 }
-
-
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(pathstring)
-	http.ServeFile(w, r, pathstring + "/src/index.html")
+	http.ServeFile(w, r, pathstring+"/src/index.html")
 }
-
-
